12abed2d34321da42a8e7a1b663e49d185c91533
Takayuki Muranushi <muranushi@gmail.com>
2013-09-25 08:32:47 +0900

    first set of working tests.

--- src/Data/Dimensions/Zahl.hs
+++ src/Data/Dimensions/Zahl.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TypeFamilies, DataKinds, TypeOperators, UndecidableInstances #-}
-{-# LANGUAGE GADTs, PolyKinds, KindSignatures #-}
+{-# LANGUAGE GADTs, PolyKinds, KindSignatures, RankNTypes, ScopedTypeVariables #-}
 
 module Data.Dimensions.Zahl where
 
@@ -8,38 +8,54 @@ import qualified GHC.TypeLits as Nat
 import Data.Dimensions.TypePrelude
 
 -- | The datatype for type-level integers
-data Zahl = Plus Nat | Minus Nat
+data Zahl = Posi Nat | Nega Nat
 
 -- | Singleton for Zahl
-data
+data instance Sing (z :: Zahl) where
+  SPosi :: forall (n :: Nat). Sing n -> Sing (Posi n) 
+  SNega :: forall (n :: Nat). Sing n -> Sing (Nega n)
+-- you don't nee forall n, it's inferred
+  
+instance forall (n::Nat) . (SingI n) => SingI (Posi n) where
+  sing = SPosi (sing :: Sing n)
+  
+instance forall (n::Nat) . (SingI n) => SingI (Nega n) where
+  sing = SNega (sing :: Sing n)
+
+instance SingE (KindParam :: KindIs Zahl) where
+  type DemoteRep (KindParam :: KindIs Zahl) = Integer
+  fromSing (SPosi n) = fromSing n
+  fromSing (SNega n) = negate $ fromSing n
+
+
 
 type family Succ (z :: Zahl) :: Zahl where
-  Succ (Plus n) = Plus (n Nat.+ 1)
-  Succ (Minus 0) = Plus 1
-  Succ (Minus 1) = Plus 0
-  Succ (Minus n) = Minus (n Nat.- 1)
+  Succ (Posi n) = Posi (n Nat.+ 1)
+  Succ (Nega 0) = Posi 1
+  Succ (Nega 1) = Posi 0
+  Succ (Nega n) = Nega (n Nat.- 1)
 
 type family Pred (z :: Zahl) :: Zahl where
-  Pred (Minus n) = Minus (n Nat.+ 1)
-  Pred (Plus 0) = Minus 0
-  Pred (Plus n) = Plus (n Nat.- 1)
+  Pred (Nega n) = Nega (n Nat.+ 1)
+  Pred (Posi 0) = Nega 0
+  Pred (Posi n) = Posi (n Nat.- 1)
 
 type family Negate (z :: Zahl) :: Zahl where
-  Negate (Minus n) = Plus n
-  Negate (Plus 0) = Plus 0
-  Negate (Plus n) = Minus n
+  Negate (Nega n) = Posi n
+  Negate (Posi 0) = Posi 0
+  Negate (Posi n) = Nega n
 
 type family (a :: Zahl) + (b :: Zahl) :: Zahl where
-  Plus n + Plus m = Plus (n Nat.+ m)
-  Minus n + Minus m = Minus (n Nat.+ m)
-  Plus n + Minus m = If (m <=? n) (Plus (n Nat.- m)) (Minus (m Nat.- n))
-  Minus m + Plus n = If (m <=? n) (Plus (n Nat.- m)) (Minus (m Nat.- n))
+  Posi n + Posi m = Posi (n Nat.+ m)
+  Nega n + Nega m = Nega (n Nat.+ m)
+  Posi n + Nega m = If (m <=? n) (Posi (n Nat.- m)) (Nega (m Nat.- n))
+  Nega m + Posi n = If (m <=? n) (Posi (n Nat.- m)) (Nega (m Nat.- n))
   
 type family (a :: Zahl) - (b :: Zahl) :: Zahl where
-  Plus n - Minus m = Plus (n Nat.+ m)
-  Minus n - Plus m = Minus (n Nat.+ m)
-  Plus n - Plus m = If (m <=? n) (Plus (n Nat.- m)) (Minus (m Nat.- n))
-  Minus m - Minus n = If (m <=? n) (Plus (n Nat.- m)) (Minus (m Nat.- n))
+  Posi n - Nega m = Posi (n Nat.+ m)
+  Nega n - Posi m = Nega (n Nat.+ m)
+  Posi n - Posi m = If (m <=? n) (Posi (n Nat.- m)) (Nega (m Nat.- n))
+  Nega m - Nega n = If (m <=? n) (Posi (n Nat.- m)) (Nega (m Nat.- n))
 
 
 
