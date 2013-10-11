8896058b0175203ac6dee428d76b47be42481091
Takayuki Muranushi <muranushi@gmail.com>
2013-10-11 12:24:24 +0900

    test for expressions (todo: put it into value-level.)

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: build
+all: test
 
 .PHONY: init build doc test install ghc-head
 
--- tests/ZahlSpec.hs
+++ tests/ZahlSpec.hs
@@ -3,11 +3,68 @@
 
 module ZahlSpec (spec) where
 
+import Control.Applicative
 import GHC.TypeLits hiding ((+)(..), (-)(..))
 import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck.Arbitrary
+import Test.QuickCheck.Gen
+
 
 import Data.Dimensions.Zahl
 
+data Expr 
+  = PZ
+  | NZ
+  | Val Integer
+  | Add Expr Expr
+  | Sub Expr Expr
+    deriving (Eq, Show)
+
+instance Arbitrary Expr where
+  arbitrary = 
+    let 
+      go n
+        | n <= 1 = 
+           oneof [ return PZ
+                 , return NZ ]
+        | otherwise = do 
+           nL <- choose (0,n)  
+           let nR = n - nL
+           oneof [ Val <$> arbitrary
+                 , Add <$> resize nL arbitrary <*> resize nR arbitrary 
+                 , Sub <$> resize nL arbitrary <*> resize nR arbitrary ]
+    in sized go 
+
+         
+  shrink (Add x y) 
+    =  [x,y] 
+    ++ (Add <$> shrink x <*> [y]) 
+    ++ (Add <$> [x] <*> shrink y)
+  shrink (Sub x y) 
+    =  [x,y] 
+    ++ (Sub <$> shrink x <*> [y]) 
+    ++ (Sub <$> [x] <*> shrink y)
+  shrink (Val n) = Val <$> shrink n
+  shrink _ = []
+
+eval :: Expr -> Integer
+eval PZ = 0
+eval NZ = 0
+eval (Val x) = x
+eval (Add x y) = eval x + eval y
+eval (Sub x y) = eval x - eval y
+
+
+eval' :: Expr -> Integer
+eval' PZ = 0
+eval' NZ = 0
+eval' (Sub (Val x) NZ) 
+  | x >= 3 = x - 2
+  | otherwise = x 
+eval' (Val x) = x
+eval' (Add x y) = eval x + eval y
+eval' (Sub x y) = eval x - eval y
 
 spec :: Spec
 spec = describe "Typelevel integer library" $ do
@@ -21,4 +78,5 @@ spec = describe "Typelevel integer library" $ do
     fromSing (sing :: Sing (Posi 30 - Posi 42)) `shouldBe` (-12)
     fromSing (sing :: Sing (Posi 30 - Nega 42)) `shouldBe` 72
     
-    
+  prop "evaluates correctly" $ \expr ->
+    eval expr == eval' expr
