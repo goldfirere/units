bf90dcc1edaece58b2caec6b1900188470640a88
Richard Eisenberg <eir@cis.upenn.edu>
2013-12-18 09:02:07 -0500

    Initial attempt at using type-nats.
    This fails because the singletons for type-nats are wrong, and
    because the solver isn't clever enough with KnownNat.

--- Data/Dimensions/Z.hs
+++ Data/Dimensions/Z.hs
@@ -9,128 +9,141 @@
 -}
 
 {-# LANGUAGE TypeFamilies, DataKinds, TypeOperators, UndecidableInstances,
-             GADTs, PolyKinds, TemplateHaskell #-}
+             GADTs, PolyKinds, TemplateHaskell, ScopedTypeVariables,
+             EmptyCase, ConstraintKinds #-}
 
 -- | This module defines a datatype and operations to represent type-level
--- integers. Though it's defined as part of the unitss package, it may be
+-- integers. Though it's defined as part of the units package, it may be
 -- useful beyond dimensional analysis. If you have a compelling non-units
 -- use of this package, please let me (Richard, @eir@ at @cis.upenn.edu@)
 -- know.
 
 module Data.Dimensions.Z where
 
-import Data.Singletons.TH
+import GHC.TypeLits
+import Data.Type.Bool
+import Data.Type.Equality
+import Data.Singletons
+import Unsafe.Coerce
 
--- | The datatype for type-level integers.
-$(singletons [d| data Z = Zero | S Z | P Z |])
+data Z = Pos Nat
+       | Neg Nat       -- this Nat should never be 0!
 
--- | Convert a 'Z' to an 'Int'
-zToInt :: Z -> Int
-zToInt Zero = 0
-zToInt (S z) = zToInt z + 1
-zToInt (P z) = zToInt z - 1
+-- | Singleton definition for integers
+data instance Sing (z :: Z) where
+  SPos :: Sing n -> Sing (Pos n)
+  SNeg :: Sing n -> Sing (Neg n)
+
+instance SingI n => SingI (Pos n) where
+  sing = SPos sing
+instance SingI n => SingI (Neg n) where
+  sing = SNeg sing
+
+instance SingKind ('KProxy :: KProxy Z) where
+  type DemoteRep ('KProxy :: KProxy Z) = Integer
+
+  fromSing (SPos n) = fromSing n
+  fromSing (SNeg n) = negate $ fromSing n
+
+  toSing n
+    | n >= 0    = withSomeSing   n  (\n' -> SomeSing $ SPos n')
+    | otherwise = withSomeSing (-n) (\n' -> SomeSing $ SNeg n')
 
 -- | Add one to an integer
-type family Succ (z :: Z) :: Z where
-  Succ Zero = S Zero
-  Succ (P z) = z
-  Succ (S z) = S (S z)
+type family Succ z where
+  Succ (Pos n) = Pos (n + 1)
+  Succ (Neg 1) = Pos 0
+  Succ (Neg n) = Neg (n - 1)
 
 -- | Subtract one from an integer
-type family Pred (z :: Z) :: Z where
-  Pred Zero = P Zero
-  Pred (P z) = P (P z)
-  Pred (S z) = z
+type family Pred z where
+  Pred (Neg n) = Neg (n + 1)
+  Pred (Pos 0) = Neg 1
+  Pred (Pos n) = Pos (n - 1)
 
 infixl 6 #+
 -- | Add two integers
-type family (a :: Z) #+ (b :: Z) :: Z where
-  Zero   #+ z      = z
-  (S z1) #+ (S z2) = S (S (z1 #+ z2))
-  (S z1) #+ Zero   = S z1
-  (S z1) #+ (P z2) = z1 #+ z2
-  (P z1) #+ (S z2) = z1 #+ z2
-  (P z1) #+ Zero   = P z1
-  (P z1) #+ (P z2) = P (P (z1 #+ z2))
+type family a #+ b where
+  (Pos a) #+ (Pos b) = Pos (a + b)
+  (Pos a) #+ (Neg b) = If (b <=? a) (Pos (a - b)) (Neg (b - a))
+  (Neg b) #+ (Pos a) = If (b <=? a) (Pos (a - b)) (Neg (b - a))
+  (Neg a) #+ (Neg b) = Neg (a + b)
 
 infixl 6 #-
 -- | Subtract two integers
-type family (a :: Z) #- (b :: Z) :: Z where
-  z      #- Zero = z
-  (S z1) #- (S z2) = z1 #- z2
-  Zero   #- (S z2) = P (Zero #- z2)
-  (P z1) #- (S z2) = P (P (z1 #- z2))
-  (S z1) #- (P z2) = S (S (z1 #- z2))
-  Zero   #- (P z2) = S (Zero #- z2)
-  (P z1) #- (P z2) = z1 #- z2
+type family a #- b where
+  (Pos a) #- (Pos b) = If (b <=? a) (Pos (a - b)) (Neg (b - a))
+  (Pos a) #- (Neg b) = Pos (a + b)
+  (Neg a) #- (Pos b) = Neg (a + b)
+  (Neg b) #- (Neg a) = If (b <=? a) (Pos (a - b)) (Neg (b - a))
 
 infixl 7 #*
 -- | Multiply two integers
-type family (a :: Z) #* (b :: Z) :: Z where
-  Zero #* z = Zero
-  (S z1) #* z2 = (z1 #* z2) #+ z2
-  (P z1) #* z2 = (z1 #* z2) #- z2
+type family a #* b where
+  (Pos 0) #* x       = Pos 0
+  x       #* (Pos 0) = Pos 0
+  (Pos a) #* (Pos b) = Pos (a * b)
+  (Pos a) #* (Neg b) = Neg (a * b)
+  (Neg a) #* (Pos b) = Neg (a * b)
+  (Neg a) #* (Neg b) = Pos (a * b)
 
 -- | Negate an integer
-type family NegZ (z :: Z) :: Z where
-  NegZ Zero = Zero
-  NegZ (S z) = P (NegZ z)
-  NegZ (P z) = S (NegZ z)
+type family NegateZ z where
+  NegateZ (Pos 0) = Pos 0
+  NegateZ (Neg n) = Pos n
+  NegateZ (Pos n) = Neg n
 
 -- | Divide two integers
-type family (a :: Z) #/ (b :: Z) :: Z where
-  Zero #/ b      = Zero
-  a    #/ (P b') = NegZ (a #/ (NegZ (P b')))
-  a    #/ b      = ZDiv b b a
+type family a #/ b where
+  (Pos 0) #/ x       = Pos 0
+  x       #/ (Pos 0) = Error "divide by 0"
+  (Pos a) #/ (Pos b) = Pos (NatDiv a b)
+  (Pos a) #/ (Neg b) = Neg (NatDiv a b)
+  (Neg a) #/ (Pos b) = Neg (NatDiv a b)
+  (Neg a) #/ (Neg b) = Pos (NatDiv a b)
 
--- | Helper function for division
-type family ZDiv (counter :: Z) (n :: Z) (z :: Z) :: Z where
-  ZDiv One n (S z')        = S (z' #/ n)
-  ZDiv One n (P z')        = P (z' #/ n)
-  ZDiv (S count') n (S z') = ZDiv count' n z'
-  ZDiv (S count') n (P z') = ZDiv count' n z'
+-- can't use If here, because type families are *eager*!
+type family NatDiv num denom where
+  NatDiv num denom = NatDivHelper (denom <=? num) num denom
 
--- | Less-than comparison
-type family (a :: Z) < (b :: Z) :: Bool where
-  Zero  < Zero   = False
-  Zero  < (S n)  = True
-  Zero  < (P n)  = False
-  (S n) < Zero   = False
-  (S n) < (S n') = n < n'
-  (S n) < (P n') = False
-  (P n) < Zero   = True
-  (P n) < (S n') = True
-  (P n) < (P n') = n < n'
+type family NatDivHelper lt num denom where
+  NatDivHelper True  num denom = 1 + NatDiv (num - denom) denom
+  NatDivHelper False num denom = 0
 
-type One   = S Zero
-type Two   = S One
-type Three = S Two
-type Four  = S Three
-type Five  = S Four
-
-type MOne   = P Zero
-type MTwo   = P MOne
-type MThree = P MTwo
-type MFour  = P MThree
-type MFive  = P MFour
+-- | Boolean less-than on Z
+type family a #< b where
+  (Neg a) #< (Pos b) = True
+  (Pos a) #< (Neg b) = False
+  (Pos a) #< (Pos b) = (a <=? b) && (Not (a == b))
+  (Neg a) #< (Neg b) = (b <=? a) && (Not (a == b))
 
 -- | This is the singleton value representing @Zero@ at the term level and
 -- at the type level, simultaneously. Used for raising units to powers.
-pZero  = SZero
-pOne   = SS pZero
-pTwo   = SS pOne
-pThree = SS pTwo
-pFour  = SS pThree
-pFive  = SS pFour
+pZero  = SPos (sing :: Sing 0)
+pOne   = SPos (sing :: Sing 1)
+pTwo   = SPos (sing :: Sing 2)
+pThree = SPos (sing :: Sing 3)
+pFour  = SPos (sing :: Sing 4)
+pFive  = SPos (sing :: Sing 5)
 
-pMOne   = SP pZero
-pMTwo   = SP pMOne
-pMThree = SP pMTwo
-pMFour  = SP pMThree
-pMFive  = SP pMFour
+pMOne   = SNeg (sing :: Sing 1)
+pMTwo   = SNeg (sing :: Sing 2)
+pMThree = SNeg (sing :: Sing 3)
+pMFour  = SNeg (sing :: Sing 4)
+pMFive  = SNeg (sing :: Sing 5)
+
+data Dict c where
+  Dict :: c => Dict c
+
+magic :: Sing n -> Dict (KnownNat n)
+magic = undefined
 
 -- | Add one to a singleton @Z@.
 pSucc :: Sing z -> Sing (Succ z)
+pSucc (SPos n) = SPos (unsafeCoerce (fromSing n + 1))
+
+
+{-
 pSucc SZero   = pOne
 pSucc (SS z') = SS (SS z')
 pSucc (SP z') = z'
@@ -144,3 +157,5 @@ pPred (SP z') = SP (SP z')
 -- | Convert a singleton @Z@ to an @Int@.
 szToInt :: Sing (z :: Z) -> Int
 szToInt = zToInt . fromSing
+
+-}
