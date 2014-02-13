6f3ee4cfdab38071c33ab836dbb79aeb52f7cfef
Takayuki Muranushi <muranushi@gmail.com>
2014-02-13 21:13:27 +0900

    addition subtraction, and multiplication of unit

--- Data/Quantity/Quantity.hs
+++ Data/Quantity/Quantity.hs
@@ -2,14 +2,20 @@
 module Data.Quantity.Quantity where
 
 import Data.Quantity.Dimension
+import Data.Quantity.Map
 import Data.Quantity.Unit
 import Data.Quantity.Zahl
 
 newtype Qu (lcsu :: [(DimK *, UnitK *)]) (unit :: [(UnitK *, Zahl)])  (value :: *) = Qu value
   deriving (Eq, Show)
 
-(|+|) :: Num val => Qu lcsu unit val -> Qu lcsu unit val -> Qu lcsu unit val 
+(|+|) :: Num val => Qu lcsu unit val -> Qu lcsu unit' val -> Qu lcsu unit val 
 (Qu x) |+| (Qu y) = Qu (x+y)
 
-(|-|) :: Num val => Qu lcsu unit val -> Qu lcsu unit val -> Qu lcsu unit val 
+(|-|) :: Num val => Qu lcsu unit val -> Qu lcsu unit' val -> Qu lcsu unit val 
 (Qu x) |-| (Qu y) = Qu (x-y)
+
+
+(|*|) :: Num val => Qu lcsu unit val -> Qu lcsu unit' val -> Qu lcsu (AddMap unit unit') val 
+(Qu x) |*| (Qu y) = Qu (x*y)
+
