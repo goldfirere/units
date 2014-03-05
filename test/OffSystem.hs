91868b40510faf8eac897d1cc335f6418d05d744
Richard Eisenberg <eir@cis.upenn.edu>
2014-03-05 15:18:00 -0500

    Add demonstration of out-of-system units in test/OffSystem.hs

---
+++ test/OffSystem.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module Test.OffSystem where
+
+import Data.Dimensions
+import Data.Dimensions.SI
+
+data Foot = Foot
+instance Unit Foot where
+  type BaseUnit Foot = Meter
+  conversionRatio _ = 0.3048
+
+data Year = Year
+instance Unit Year where
+  type BaseUnit Year = Second
+  conversionRatio _ = 60 * 60 * 24 * 365.242
+
+vel :: Velocity
+vel = 1e6 % (Foot :/ Year)
+
+velInMS :: Double
+velInMS = vel # (Meter :/ Second)
