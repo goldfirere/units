0ff2d454ac91277f5afac46997a572429a762dde
Takayuki Muranushi <muranushi@google.com>
2014-07-11 11:26:59 +0900

    some test cases on Vector.

--- Tests/Vector.hs
+++ Tests/Vector.hs
@@ -1,12 +1,15 @@
-{- Test the property of quantity as vector space
+{-# LANGUAGE TypeFamilies #-}
+
+{- Test the property of vector quantity 
    Copyright (c) 2014 Richard Eisenberg
 -}
 
 module Tests.Vector where
 
-import Data.Metrology.Poly
+import Data.Metrology.Vector
 import Data.Metrology.Show ()
 import Data.Metrology.SI.Poly
+import Data.VectorSpace
 
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -15,42 +18,27 @@ len1 :: Length SI Rational
 len1 = 1 % Meter
 
 
-{-
+type V2 = Length SI (Rational, Rational)    
+vec1 :: V2
+vec1 = (2,3) % Meter
 
-Since the vector space with units are as follows:
-
-    -- | Multiply a quantity by a scalar from the right
-    (|*) :: VectorSpace n => Qu a l n -> Scalar n -> Qu a l n
-    (Qu a) |* b = Qu (a ^* b)
-
-An example of 2d vector with unit signature will be as follows:
-
-    type V2 = Length SI (Rational, Rational)    
-    coord1 :: V2
-    coord1 = (2,3) % Meter
-
-But then we encounter erros like follows:
-
-    Tests/Vector.hs:20:16:
-        No instance for (Fractional (Rational, Rational))
-          arising from a use of ‘%’
-        In the expression: (2, 3) % Meter
-        In an equation for ‘coord1’: coord1 = (2, 3) % Meter
-
--}
-
-
-linearCompose :: (Fractional a) => a -> a -> Qu d l a  -> Qu d l a -> Qu d l a 
+linearCompose :: (VectorSpace n, a ~ Scalar n) => a -> a -> Qu d l n  -> Qu d l n -> Qu d l n
 linearCompose a b x y = a *| x |+| b *| y
 
 
 tests :: TestTree
 tests = testGroup "Show"
-  [ testCase "Identity" $ len1 @?= len1
-  , testCase "Addition" $ len1 |+| len1 @?= 2 *| len1 
+  [ testCase "Identity" $ vec1 @?= vec1
+  , testCase "Addition" $ vec1 |+| vec1 @?= 2 *| vec1 
   , testCase "Summation of multiple quantities" $ 
-      qSum [len1,len1,len1] @?= 3 *| len1 
+      qSum [vec1,vec1,vec1] @?= 3 *| vec1 
   , testCase "Linear composition" $ 
-      linearCompose 0.2 0.8 len1 len1 @?= len1
+      linearCompose 0.2 0.8 vec1 vec1 @?= vec1
+
+  , testCase "Multiplication from right" $ 4 *| vec1 @?= vec1 |* 4
+  , testCase "Division by scalar" $ 10 *| vec1 @?= vec1 |/ 0.1
+
+  , testCase "scalar product" $
+      (5 % Meter) |*^| vec1 @?= ((10, 15) % (Meter :^ sTwo))
   ]
 
