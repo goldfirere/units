702c3dc1a2277e550836f1ad23ce8c3a3baa8249
Adam Gundry <adam@well-typed.com>
2014-11-01 13:08:19 +0000

    Move parser to a separate package (#36)

--- Tests/Parser.hs
+++ Tests/Parser.hs
@@ -9,12 +9,8 @@ module Tests.Parser where
 import Prelude hiding ( lex, exp )
 
 import Data.Metrology.Parser
-import Data.Metrology.Parser.Internal
 import Data.Metrology.SI
 
-import Control.Monad.Reader
-import qualified Data.Map.Strict as Map
-import Text.Parsec
 import Language.Haskell.TH
 import Data.Generics
 
@@ -35,83 +31,10 @@ stripModules = everywhere (mkT (mkName . nameBase))
 pprintUnqualified :: (Ppr a, Data a) => a -> String
 pprintUnqualified = pprint . stripModules
 
-----------------------------------------------------------------------
--- Lexer
-----------------------------------------------------------------------
-
-lexTest :: String -> String
-lexTest s =
-  case lex s of
-    Left _     -> "error"
-    Right toks -> show toks
-
-lexTestCases :: [(String, String)]
-lexTestCases = [ ( "m", "[m]" )
-               , ( "", "[]" )
-               , ( "m s", "[m,s]" )
-               , ( "   m   s ", "[m,s]" )
-               , ( "m   ", "[m]" )
-               , ( "   m", "[m]" )
-               , ( "( m  /s", "[(,m,/,s]" )
-               , ( "!", "error" )
-               , ( "1 2 3", "[1,2,3]" )
-               , ( "  ", "[]" )
-               ]
-
-lexTests :: TestTree
-lexTests = testGroup "Lexer" $
-  map (\(str, out) -> testCase ("`" ++ str ++ "'") $ lexTest str @?= out) lexTestCases
-
-----------------------------------------------------------------------
--- Unit strings
-----------------------------------------------------------------------
-
-unitStringTestCases :: [(String, String)]
-unitStringTestCases = [ ("m", "Meter")
-                      , ("s", "Second")
-                      , ("min", "Minute")
-                      , ("km", "Kilo :@ Meter")
-                      , ("mm", "Milli :@ Meter")
-                      , ("kmin", "Kilo :@ Minute")
-                      , ("dam", "error")   -- ambiguous!
-                      , ("damin", "Deca :@ Minute")
-                      , ("ms", "Milli :@ Second")
-                      , ("mmin", "Milli :@ Minute")
-                      , ("mmm", "error")
-                      , ("mmmin", "error")
-                      , ("sm", "error")
-                      , ("", "error")
-                      , ("dak", "error")
-                      , ("das", "Deca :@ Second")
-                      , ("ds", "Deci :@ Second")
-                      , ("daam", "Deca :@ Ampere")
-                      , ("kam", "Kilo :@ Ampere")
-                      , ("dm", "Deci :@ Meter")
-                      ]
-
-parseUnitStringTest :: String -> String
-parseUnitStringTest s =
-  case flip runReader testSymbolTable $ runParserT unitStringParser () "" s of
-    Left _ -> "error"
-    Right exp -> show exp
-
-unitStringTests :: TestTree
-unitStringTests = testGroup "UnitStrings" $
-  map (\(str, out) -> testCase ("`" ++ str ++ "'") $ parseUnitStringTest str @?= out)
-    unitStringTestCases
 
 ----------------------------------------------------------------------
 -- Symbol tables
 ----------------------------------------------------------------------
-
-mkSymbolTableTests :: TestTree
-mkSymbolTableTests = testGroup "mkSymbolTable"
-  [ testCase "Unambiguous1" (Map.keys (prefixTable testSymbolTable) @?= ["d","da","k","m"])
-  , testCase "Unambiguous2" (Map.keys (unitTable testSymbolTable) @?= ["am","m","min","s"])
-  , testCase "AmbigPrefix" (leftOnly (mkSymbolTable [("a",''Milli),("a",''Centi)] ([] :: [(String,Name)])) @?= Just "The label `a' is assigned to the following meanings:\n[\"Data.Metrology.SI.Prefixes.Milli\",\"Data.Metrology.SI.Prefixes.Centi\"]\nThis is ambiguous. Please fix before building a unit parser.")
-  , testCase "AmbigUnit" (leftOnly (mkSymbolTable ([] :: [(String,Name)]) [("m",''Meter),("m",''Minute)]) @?= Just "The label `m' is assigned to the following meanings:\n[\"Data.Metrology.SI.Units.Meter\",\"Data.Metrology.SI.Units.Minute\"]\nThis is ambiguous. Please fix before building a unit parser.")
-  , testCase "MultiAmbig" (leftOnly (mkSymbolTable [("a",''Milli),("b",''Centi),("b",''Deci),("b",''Kilo),("c",''Atto),("c",''Deca)] [("m",''Meter),("m",''Minute),("s",''Second)]) @?= Just "The label `b' is assigned to the following meanings:\n[\"Data.Metrology.SI.Prefixes.Centi\",\"Data.Metrology.SI.Prefixes.Deci\",\"Data.Metrology.SI.Prefixes.Kilo\"]\nThe label `c' is assigned to the following meanings:\n[\"Data.Metrology.SI.Prefixes.Atto\",\"Data.Metrology.SI.Prefixes.Deca\"]\nThis is ambiguous. Please fix before building a unit parser.")
-                                                                                                ]
 
 testSymbolTable :: SymbolTable Name Name
 Right testSymbolTable =
@@ -206,9 +129,6 @@ parseUnitTestsT = testGroup "ParseUnitType" $
 
 tests :: TestTree
 tests = testGroup "Parser"
-  [ lexTests
-  , mkSymbolTableTests
-  , unitStringTests
-  , parseUnitTests
+  [ parseUnitTests
   , parseUnitTestsT
   ]
