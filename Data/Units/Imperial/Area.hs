34795d20c8ff4809cf6ade8e37916b3c6539f4df
ARJANEN Loïc Jean David <arjanen.loic@gmail.com>
2017-11-11 19:09:34 +0700

    Adding UK imperial units.
    Adding the UK imperial units under Data.Units.Imperial. For the weight units, use the British convention to use hunderweight for the long one and cental for the short one. Accordingly, ton is the long one. Also, the nautical units use the old Admiralty definition (1 NM = 6080 ft)

---
+++ Data/Units/Imperial/Area.hs
@@ -0,0 +1,33 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Units.Imperial.Area
+-- Copyright   :  (C) 2013 Richard Eisenberg
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  Richard Eisenberg (eir@cis.upenn.edu)
+-- Stability   :  experimental
+-- Portability :  non-portable
+--
+-- The traditional area units are defined in reference to the survey
+-- ones.
+--
+-- Where possible, reference have been made to UK legislation. However,
+-- Wikipedia's base is /much/ better organized than any government
+-- resource immediately available.
+-----------------------------------------------------------------------------
+
+module Data.Units.Imperial.Area where
+
+import Data.Metrology.TH
+
+import Data.Units.Imperial.Length
+import Data.Units.Imperial.Survey
+
+import Language.Haskell.TH
+
+declareDerivedUnit "Perch" [t| Rod :* Rod       |] 1 (Just "perch")
+declareDerivedUnit "Rood"  [t| Furlong :* Rod   |] 1 (Just "rood")
+declareDerivedUnit "Acre"  [t| Furlong :* Chain |] 1 (Just "acre")
+
+-- | Areas units: All units above
+areas :: [Name]
+areas = [ ''Perch, ''Rood, ''Acre ]
---
+++ Data/Units/Imperial/Length.hs
@@ -0,0 +1,48 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Units.Imperial.Length
+-- Copyright   :  (C) 2013 Richard Eisenberg
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  Richard Eisenberg (eir@cis.upenn.edu)
+-- Stability   :  experimental
+-- Portability :  non-portable
+--
+-- Due to the <https://en.wikipedia.org/wiki/International_yard_and_pound
+-- International yard and pound agreement of 1959>, it so happens that
+-- Imperial and US customary units of length have the same base (the
+-- international yard at 0.9144 meters). However, if subdivisions are
+-- generally the same, Imperial units feature multiples of the yard that
+-- US customary ones generally don't (e.g. the Imperial furlong is defined
+-- by reference to the international yard, not the US survey one).
+--
+-- Where possible, reference have been made to UK legislation. However,
+-- Wikipedia's base is /much/ better organized than any government
+-- resource immediately available.
+-----------------------------------------------------------------------------
+
+module Data.Units.Imperial.Length where
+
+import Data.Metrology.TH
+
+import qualified Data.Units.US.Misc as US
+
+import Language.Haskell.TH
+
+declareDerivedUnit "Thou"    [t| US.Mil  |] 1  (Just "th")
+declareDerivedUnit "Inch"    [t| US.Inch |] 1  (Just "in")
+declareDerivedUnit "Foot"    [t| US.Foot |] 1  (Just "ft")
+declareDerivedUnit "Yard"    [t| US.Yard |] 1  (Just "yd")
+declareDerivedUnit "Chain"   [t| Yard    |] 22 (Just "ch")
+declareDerivedUnit "Furlong" [t| Chain   |] 10 (Just "fur")
+declareDerivedUnit "Mile"    [t| US.Mile |] 1  (Just "mi")
+declareDerivedUnit "League"  [t| Mile    |] 3  (Just "lea")
+
+declareDerivedUnit "Hand" [t| Inch |] 4 (Just "th")
+
+-- | Common lengths units: 'Foot', 'Inch', 'Yard', and 'Mile'
+commonLengths :: [Name]
+commonLengths = [ ''Foot, ''Inch, ''Yard, ''Mile ]
+
+-- | Other lengths units: 'Thou', 'Hand', 'Chain', 'Furlong' and 'League'
+otherLengths :: [Name]
+otherLengths = [ ''Thou, ''Hand, ''Chain, ''Furlong, ''League ]
